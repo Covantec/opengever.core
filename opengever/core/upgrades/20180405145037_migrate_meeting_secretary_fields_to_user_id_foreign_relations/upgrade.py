fe0311a8c95e73a2343d3ce74cd30cadb3202065
Joni Orponen <j.orponen@4teamwork.ch>
2018-03-23 12:25:46 +0100

    Change the meeting secretary foreign key to user id.

---
+++ opengever/core/upgrades/20180405145037_migrate_meeting_secretary_fields_to_user_id_foreign_relations/upgrade.py
@@ -0,0 +1,49 @@
+from opengever.core.upgrade import SchemaMigration
+from opengever.meeting.model import Member
+from opengever.ogds.models import USER_ID_LENGTH
+from opengever.ogds.models.user import User
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.sql.expression import column
+from sqlalchemy.sql.expression import table
+
+
+class MigrateMeetingSecretaryFieldsToUserIdForeignRelations(SchemaMigration):
+    """Migrate meeting secretary fields to user id foreign relations."""
+
+    def migrate(self):
+        # Add a temp secretary id column to meetings
+        new_secretary_id = Column('new_secretary_id', String(USER_ID_LENGTH), ForeignKey(User.userid))
+        self.op.add_column('meetings', new_secretary_id)
+        # Populate temp secretary column - user if match else new inactive user
+        meetings_table = table(
+            'meetings',
+            column('id'),
+            column('secretary_id'),
+            column('new_secretary_id'),
+            )
+        meetings = self.connection.execute(meetings_table.select()).fetchall()
+        for meeting in meetings:
+            if meeting.secretary_id:
+                member = Member.query.filter(Member.member_id == meeting.secretary_id).first()
+                if member:
+                    user = User.query.filter(User.firstname == member.firstname, User.lastname == member.lastname).first()
+                    if not user:
+                        # We have to create an inactive user for the member
+                        userid = '{}.{}'.format(member.firstname.lower(), member.lastname.lower())[:USER_ID_LENGTH]
+                        user = User(userid)
+                        user.active = False
+                        user.firstname = member.firstname
+                        user.lastname = member.lastname
+                        user.email = member.email
+                        self.session.add(user)
+                    self.execute(
+                        meetings_table.update()
+                        .values(new_secretary_id=user.userid)
+                        .where(meetings_table.columns.id == meeting.id)
+                        )
+        # Drop old secretary column
+        self.op.drop_column('meetings', 'secretary_id')
+        # Rename temp secretary column
+        self.op.alter_column('meetings', 'new_secretary_id', new_column_name='secretary_id')
