ac8e5cb195be4815a6753b4222d2a1ace5ddccd9
Pablo Verges <pablo.verges@4teamwork.ch>
2018-03-26 10:22:28 +0200

    Fix redirector test

--- opengever/tabbedview/tests/test_redirectors.py
+++ opengever/tabbedview/tests/test_redirectors.py
@@ -1,6 +1,8 @@
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import statusmessages
 from opengever.testing import IntegrationTestCase
+import opengever
+import os
 
 
 class RedirectorTests(IntegrationTestCase):
@@ -9,11 +11,16 @@ class RedirectorTests(IntegrationTestCase):
     def test_sablon_template_redirector(self, browser):
         self.login(self.administrator, browser)
         browser.open(self.templates, view='++add++opengever.meeting.sablontemplate')
-
-        browser.fill({
-            'Title': u'Sablonv\xferlage',
-            'File': ('Sablon Template', 'sablon_template.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'),
-        }).save()
+        valid_template_path = os.path.join(
+            os.path.dirname(opengever.testing.assets.__file__),
+            'valid_sablon_template.docx'
+        )
+        assert os.path.exists(valid_template_path)
+        with open(valid_template_path) as sablon_template:
+            browser.fill({
+                'Title': u'Sablonv\xferlage',
+                'File': sablon_template,
+            }).save()
         statusmessages.assert_no_error_messages()
 
         self.assertEquals('http://nohost/plone/vorlagen#sablontemplates-proxy',
